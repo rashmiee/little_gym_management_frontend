0d15fceb5df0ea44c9a9250b67d0c15a8fdd87eb
Rashmi Prabha <rashmiprabha1997@gmail.com>
2024-04-29 18:14:49 +0530

    user page authorization added

--- src/components/RouterPage.js
+++ src/components/RouterPage.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
-import Login  from './Login';
-import Registration  from './Registration';
+// Import your components
+import Login from './Login';
+import Registration from './Registration';
 // Dashboards
 import UserDashboard from './Dasboards/UserDashboard';
 import AdminDashboard from './Dasboards/AdminDashboard';
@@ -10,9 +11,9 @@ import TeacherDashboard from './Dasboards/TeacherDashboard';
 // Resgistrations
 import TeacherRegistration from './Admin/TeacherRegistration';
 import ChildRegistration from './User/ChildRegistration';
-// ClassSeeion
+// ClassSession
 import AddClassSession from './Admin/AddClassSession';
-import AddLessonToClassSession from './Teacher/AddLessonToClassSession'
+import AddLessonToClassSession from './Teacher/AddLessonToClassSession';
 // Lesson
 import AddLesson from './Teacher/AddLesson';
 // Skill
@@ -20,30 +21,55 @@ import AddSkill from './Teacher/AddSkill';
 // SkillProgress
 import SkillProgress from './Teacher/SkillProgress';
 
-export default function RouterPage()
-{
+export default function RouterPage() {
+  const [userType, setUserType] = useState('');
+
+  useEffect(() => {
+    // Get user type from local storage
+    const storedUserType = localStorage.getItem('userType');
+    setUserType(storedUserType);
+  }, []);
+
+  // Render routes based on user type
+  const renderRoutes = () => {
+    return (
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/registration" element={<Registration />} />
+        {/* Admin routes */}
+        {userType === 'Admin' && (
+          <>
+            <Route path="/adminDashboard" element={<AdminDashboard />} />
+            <Route path="/teacherRegistration" element={<TeacherRegistration />} />
+            <Route path="/addClassSession" element={<AddClassSession />} />
+          </>
+        )}
+        {/* Teacher routes */}
+        {userType === 'Teacher' && (
+          <>
+            <Route path="/teacherDashboard" element={<TeacherDashboard />} />
+            <Route path="/addLessonToClassSession" element={<AddLessonToClassSession />} />
+            <Route path="/addLesson" element={<AddLesson />} />
+            <Route path="/addSkill" element={<AddSkill />} />
+            <Route path="/skillProgress" element={<SkillProgress />} />
+          </>
+        )}
+        {/* User routes */}
+        {userType === 'User' && (
+          <>
+            <Route path="/userDashboard" element={<UserDashboard />} />
+            <Route path="/childRegistration" element={<ChildRegistration />} />
+          </>
+        )}
+        {/* Redirect to dashboard if unauthorized */}
+        <Route path="*" element={<Navigate to={`/${userType.toLowerCase()}Dashboard`} />} />
+      </Routes>
+    );
+  };
+
   return (
     <Router>
-      <Routes>
-        <Route path="/" exact element={<Login />} />
-        <Route path="/registration" element={<Registration />} />
-        {/* Dashboards */}
-        <Route path="/userDashboard" element={<UserDashboard />} />
-        <Route path="/adminDashboard" element={<AdminDashboard />} />
-        <Route path="/teacherDashboard" element={<TeacherDashboard />} />
-        {/* Resgistrations */}
-        <Route path="/teacherRegistration" element={<TeacherRegistration />} />
-        <Route path="/childRegistration" element={<ChildRegistration />} />
-        {/* ClassSession */}
-        <Route path="/addClassSession" element={<AddClassSession />} />
-        <Route path="/addLessonToClassSession" element={<AddLessonToClassSession />} />
-        {/* Lesson */}
-        <Route path="/addLesson" element={<AddLesson />} />
-        {/* Skill */}
-        <Route path="/addSkill" element={<AddSkill />} />
-        {/* SkillProgress */}
-        <Route path="/skillProgress" element={<SkillProgress />} />
-      </Routes>
+      {renderRoutes()}
     </Router>
   );
 }
