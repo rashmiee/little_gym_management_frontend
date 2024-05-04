329be695a677103a0f03679795b14868f67ed857
Rashmi Prabha <rashmiprabha1997@gmail.com>
2024-05-04 15:23:40 +0530

    fixed Skills UI

--- src/components/Teacher/AddSkill.js
+++ src/components/Teacher/AddSkill.js
@@ -1,6 +1,7 @@
 import React, { Fragment, useState, useEffect } from "react";
 import axios from 'axios';
 import TeacherHeader from "../Dasboards/TeacherHeader";
+import EditSkillModal from "./EditSkillPopup";
 
 export default function AddSkill() {
   const [skills, setSkills] = useState([]);
@@ -8,6 +9,51 @@ export default function AddSkill() {
   const [description, setDescription] = useState('');
   const [isNameValid, setIsNameValid] = useState(true);
   const [isDescriptionValid, setIsDescriptionValid] = useState(true);
+
+  const [selectedSkill, setSelectedSkill] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openEditModal = (skill) => {
+    setSelectedSkill(skill);
+    setIsModalOpen(true);
+  };
+
+  const closeEditModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleUpdate = (updatedSkill) => {
+    console.log(updatedSkill);
+    axios
+      .put(`/api/Skill/${updatedSkill.skill_ID}`, updatedSkill)
+      .then((result) => {
+        alert(result.data.statusMessage);
+        closeEditModal();
+        fetchSkills(); // Fetch updated list of Skill
+      })
+      .catch((error) => {
+        alert("Error updating Skill. Please try again.");
+      });
+  };
+
+  const confirmDelete = (skill) => {
+    if (
+      window.confirm(
+        `Are you sure you want to delete ${skill.name} ?`
+      )
+    ) {
+      const url = `/api/Skill/${skill.skill_ID}`;
+      axios
+        .delete(url)
+        .then((result) => {
+          alert(result.data.statusMessage);
+          fetchSkills();
+        })
+        .catch((error) => {
+          alert("Error deleting Skill. This Skill is already added to the Progress.");
+        });
+    }
+  };
 
   useEffect(() => {
     fetchSkills();
@@ -67,6 +113,77 @@ export default function AddSkill() {
     <Fragment>
       <TeacherHeader />
       {/* Skill Add Form */}
+      <section>
+        <div className="testbox">
+          <form>
+            <div className="banner">
+              <h1>Skill Creation</h1>
+            </div>
+            <br/>
+            <fieldset>
+              <div className="column-custom">
+              <div
+              data-mdb-input-init
+              className={`form-outline mb-4 ${
+                isNameValid ? "" : "has-invalid"
+              }`}
+            >
+              <input
+                type="text"
+                id="txtName"
+                className="form-control form-control-lg"
+                onChange={(e) => handleNameChange(e.target.value)}
+                value={name}
+              />
+              <label
+                className="form-label d-block text-center"
+                htmlFor="txtName"
+              >
+                Name{" "}
+                {isNameValid
+                  ? <span className="span-red">*</span>
+                  : "(required - Please enter the skill name)"}
+              </label>
+            </div>
+            <div
+              data-mdb-input-init
+              className={`form-outline mb-4 ${
+                isDescriptionValid ? "" : "has-invalid"
+              }`}
+            >
+              <textarea
+                id="txtDescription"
+                className="form-control form-control-lg"
+                onChange={(e) => handleDescriptionChange(e.target.value)}
+                value={description}
+              />
+              <label
+                className="form-label d-block text-center"
+                htmlFor="txtDescription"
+              >
+                Description{" "}
+                {isDescriptionValid
+                  ? <span className="span-red">*</span>
+                  : "(required - Please enter the skill description)"}
+              </label>
+            </div>
+            <div className="d-flex justify-content-center pt-3">
+              <button
+                type="button"
+                data-mdb-button-init
+                data-mdb-ripple-init
+                className="btn btn-dark btn-lg btn-block"
+                onClick={handleSave}
+              >
+                Submit form
+              </button>
+            </div>
+              </div>
+            </fieldset>
+          </form>
+        </div>
+      </section>
+
       <section>
         <div className="card card-registration my-4">
           <div className="card-body p-md-5 text-black">
@@ -143,14 +260,17 @@ export default function AddSkill() {
       </section>
       {/* Skill Table */}
       <section>
-        <div className="card card-registration my-4">
-          <div className="card-body p-md-5 text-black">
-            <h3 className="mb-5 text-uppercase text-center">Skills</h3>
-            <table className="table">
+        <div className="testbox">
+          <form>
+            <div class="banner">
+              <h1>Skills</h1>
+            </div>
+            <table className="table table-striped custome-table-style">
               <thead>
                 <tr>
                   <th scope="col">Skill Name</th>
                   <th scope="col">Description</th>
+                  <th scope="col">Actions</th>
                 </tr>
               </thead>
               <tbody>
@@ -158,12 +278,37 @@ export default function AddSkill() {
                   <tr key={index}>
                     <td>{skill.name}</td>
                     <td>{skill.description}</td>
+                    <td>
+                      <button
+                        type="button"
+                        className="edit-btn-style"
+                        onClick={() => openEditModal(skill)}
+                      >
+                        <i className="fa fa-edit"></i>
+                      </button>
+                      <button
+                        type="button"
+                        className="delete-btn-style"
+                        onClick={() => {
+                          confirmDelete(skill);
+                        }}
+                      >
+                        <i className="fa fa-trash"></i>
+                      </button>
+                    </td>
                   </tr>
                 ))}
               </tbody>
             </table>
-          </div>
+          </form>
         </div>
+        {isModalOpen && (
+          <EditSkillModal
+            skill={selectedSkill}
+            onClose={closeEditModal}
+            onUpdate={handleUpdate}
+          />
+        )}
       </section>
     </Fragment>
   );
